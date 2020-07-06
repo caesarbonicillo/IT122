b1e247dd7ed23c4193513da6212bead4dda768ad
david <caesarbonicillo@gmail.com>
2020-07-05 23:05:41 -0700

    2nd commit

--- data.js
+++ data.js
@@ -1,1 +1,13 @@
+// Create array of 5 objects with 4 attributes each
+const Films = [
+    {title : 'Spotlight', Dir: 'Tom McCarthy', year: 2015,, Oscars: '2 awards'},
+    {title: 'Moonlight', Dir: 'Barry Jenkins', year: 2016, Oscars: '3 awards'},
+    {title: 'Shape of Water', Dir: 'Guillermo del Toro', year: 2017, Oscars: '4 awards'},
+    {title: 'Green Book', Dir: 'Peter Farelly', year: 2018, Oscars: '3 awards'},
+    {title: 'Parasite', Dir: 'Bong Joon-hoo', year: 2019, Oscars: '4 awards'}
+];
 
+// an exported getAll method that returns all array items
+exports.getAll = () => {
+    return Films;
+}
--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// Create variables for http and data/books
+const http = require("http");
+const bookFilms = require("./data");
 
+// Create variable to get data from data.js
+let showFilms = Films.getAll();
+
+const http = require("http"); 
+http.createServer((req,res) => {
+  const path = req.url.toLowerCase();
+  switch(path) {
+    case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Home page');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
