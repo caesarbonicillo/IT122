c4a148750c0d4e7a44317b46d4383fd768eaaae9
david <caesarbonicillo@gmail.com>
2020-07-12 16:32:32 -0700

    add home, detail handlebars, views folder

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 // Create array of 5 objects with 4 attributes each
 const Films = [
-    {title : 'Spotlight', Dir: 'Tom McCarthy', year: 2015,, Oscars: '2 awards'},
-    {title: 'Moonlight', Dir: 'Barry Jenkins', year: 2016, Oscars: '3 awards'},
-    {title: 'Shape of Water', Dir: 'Guillermo del Toro', year: 2017, Oscars: '4 awards'},
-    {title: 'Green Book', Dir: 'Peter Farelly', year: 2018, Oscars: '3 awards'},
-    {title: 'Parasite', Dir: 'Bong Joon-hoo', year: 2019, Oscars: '4 awards'}
+    {title : 'Spotlight', dir: 'Tom McCarthy', year: 2015,, oscars: '2 awards'},
+    {title: 'Moonlight', dir: 'Barry Jenkins', year: 2016, oscars: '3 awards'},
+    {title: 'Shape of Water', dir: 'Guillermo del Toro', year: 2017, oscars: '4 awards'},
+    {title: 'Green Book', dir: 'Peter Farelly', year: 2018, oscars: '3 awards'},
+    {title: 'Parasite', dir: 'Bong Joon-hoo', year: 2019, oscars: '4 awards'}
 ];
 
 // an exported getAll method that returns all array items
