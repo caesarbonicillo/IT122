1c3049223ffb2cb6197dd85f5c7e9049ef2f6e77
david <caesarbonicillo@gmail.com>
2020-08-14 23:35:00 -0700

    Assignment 3 adds Mocha, Chai, Test folder/file testing

---
+++ test/test.js
@@ -0,0 +1,41 @@
+const expect = require("chai").expect;
+const dogs = require("../data");
+const data = require("../data");
+
+const displayFilms = data.getAll();
+
+describe("Film module", () => {
+    it("Returns requested Film", () => {
+        const result = data.getDetail("Spotlight");
+        expect(result).to.deep.equal({title: 'Spotlight', Dir: 'Tom McCarthy', year: 2015, Oscars: '2 awards', id: 0});
+    })
+
+    it("Fails w/invalid film"), () => {
+        const result = data.getDetail("Lion King");
+        expect(result.msg).to.deep.equal("Lion King not a Winner");
+    };
+
+    it("Adds a new Film", () => {
+        data.addFilm("Frozen", "Jennifer Lee", 2013, "0 awards", 6);
+        expect(displayFilms).to.deep.include({title: 'Frozen', Dir: 'Jennifer Lee', year: 2013, Oscars: '0 awards', id: 6});
+    })
+
+    it("Fails to add due to missing params", () => {
+        data.addFilm("fake film", "Someone");
+        expect(displayFilms).to.not.include({title: "fake film", "Someone", year: undefined, Oscars: undefined, id: undefined});
+        expect(data.addFilm("fake film", "Someone").msg).to.deep.equal('incomplete info');
+    });
+
+    it("Deletes requested Movie", () => {
+        data.delFilm("Spotlight");
+        expect(displayFilms).to.not.include({title: 'Spotlight', Dir: 'Tom McCarthy', year: 2015, Oscars: '2 awards', id: 0});
+    });
+
+    it("Delete failed to delete. No matches", () => {
+        data.delFilm("Aladdin");
+        expect(data.getDetail("Aladdin").msg).to.deep('"Aladdin" not found');
+    })
+
+    
+
+});
